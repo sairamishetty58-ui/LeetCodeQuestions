e6e3c122a4a460e6199142cc4f0670c1b9c68ebe
sairamishetty58-ui <sairamishetty58@gmail.com>
2025-10-05 12:14:16 +1100

    Create containsDuplicate.py

---
+++ containsDuplicate.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        Time Complexity: O(n), where n is the length of list
+        Space Complexity: O(n), where n is the length of list, because extra space of size N is allocated to the set in the worst case where the first and last numbers are duplicates or no duplicates exist
+        Inputs:
+        nums: List, a list of all values which are being expected.
+        Output:
+        returns a boolean if a dupliate exist
+        """
+        # Solution 1: Check if the length of set is matching the normal length but still creates the set and fills the values behind.
+        return len(set(nums))!=len(nums)
+        # Solution 2: Alternate method which manually appends to the set
+        result_set = set()
+        for i in nums:
+            if i in result_set:
+                return True
+            result_set.add(i)
+        return False
